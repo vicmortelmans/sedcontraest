04ee5860bcd99bc1490f2e8ed28f256cb17ecb9a
Vic Mortelmans <vicmortelmans@gmail.com>
2024-09-23 21:52:00 +0200

    upgrade to python 3.12 and storing cookies and login working, but no mentions

--- sedcontraest.py
+++ sedcontraest.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-from sys import stdout
+import asyncio
 from dotenv import load_dotenv
 import logging
 from openai import OpenAI
 import os
+from sys import stdout
 import tweepy
+from twikit import Client
 
 logger = logging.getLogger("sedcontraest")
 handler = logging.StreamHandler(stdout)
@@ -15,7 +17,7 @@ logger.setLevel(logging.DEBUG)
 load_dotenv(os.getenv('HOME') + "/.env")  # contains OPENAI_API_KEY
 AI = OpenAI()
 
-# Authenticate to Twitter
+# Authenticate to Twitter API
 key = ""
 keySecret = " "
 accessToken = " "
@@ -23,6 +25,22 @@ accessTokenSecret = " "
 bearer_token=None
 client = tweepy.Client(None,os.environ["X_API_KEY"],os.environ["X_API_KEY_SECRET"],os.environ["X_ACCESS_TOKEN"],os.environ["X_ACCESS_TOKEN_SECRET"])
 client.get_me() 
+
+# Authenticate to Twitter scraper
+client = Client('en-US')
+
+async def main():
+    #await client.login(
+    #    auth_info_1=os.environ["X_USERNAME"],
+    #    auth_info_2=os.environ["X_EMAIL"],
+    #    password=os.environ["X_PASSWORD"]
+    #)
+    #client.save_cookies('twikit-cookies.json')
+    client.load_cookies('twikit-cookies.json')
+    notifications = await client.get_notifications('All')
+    for notification in notifications:
+        print(notification.message, notification.tweet, notification.from_user)
+
 
 def answer(statement):
     messages = [
@@ -37,4 +55,5 @@ def answer(statement):
 
 
 #print(answer("life is short"))
-client.create_tweet(text=answer("life is short"))
+#client.create_tweet(text=answer("life is short"))
+asyncio.run(main())
